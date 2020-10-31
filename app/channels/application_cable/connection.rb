6b12327295cea318a3fda8cb9a09553597ed9cf6
David Whitlatch <davidtwhitlatch@gmail.com>
2020-10-31 00:05:57 -0400

    init

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,19 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :username
+
+    def connect
+      self.username = verified_room
+    end
+
+    private
+
+    def verified_room
+      if Room.find_by_code(params[:code])
+        params[:username]
+      else
+        reject_unauthorized_connection
+      end
+    end
+  end
+end
